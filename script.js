3c3ed3c8d3cf9dac39e7fb4c2b36947702964331
jordanj2k00 <jordanj2k00@gmail.com>
2026-04-16 16:07:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const pieceImages = {
+    p: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bp.png",
+    r: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/br.png",
+    n: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bn.png",
+    b: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bb.png",
+    q: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bq.png",
+    k: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bk.png",
+
+    P: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wp.png",
+    R: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wr.png",
+    N: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wn.png",
+    B: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wb.png",
+    Q: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wq.png",
+    K: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wk.png"
+};
 let currentMode = "computer";
 
 const chess = new Chess();
